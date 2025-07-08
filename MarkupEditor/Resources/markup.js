97be524afbaa79e825457df9a3bc1c2a130f9678
antoine chinault <antoine@wizzcad.com>
2025-07-08 16:36:03 +0200

    applyAutoLink check alreadyMarked

--- MarkupEditor/Resources/markup.js
+++ MarkupEditor/Resources/markup.js
@@ -18253,62 +18253,72 @@
   //  Created by Antoine CHINAULT on 24/06/2025.
   //
 
-  function applyAutoLink(view) {
+    function applyAutoLink(view) {
       const { state } = view;
       const { tr } = state;
       let modified = false;
       const linkMark = state.schema.marks.link;
       if (!linkMark) return;
-      
+
       const textLinkPatterns = [
-          {
-              type: 'url',
-              regex: /\b(?:https?:\/\/)?(?:www\.)?[a-z0-9\-._~%]+(?:\.[a-z]{2,})(?:\/[^\s]*)?\b/gi,
-              getHref: (match) => match.startsWith('http') ? match : `https://${match}`
-          },
-          {
-              type: 'email',
-              regex: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
-              getHref: (match) => `mailto:${match}`
-          },
-          {
-              type: 'phone',
-              regex: /\b\+?[0-9]{1,3}?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)*\d{3,4}[-.\s]?\d{3,4}\b/g,
-              getHref: (match) => `tel:${match.replace(/[^\d+]/g, '')}`
-          }
+        {
+          type: 'url',
+          regex: /\b(?:https?:\/\/)?(?:www\.)?[a-z0-9\-._~%]+(?:\.[a-z]{2,})(?:\/[^\s]*)?\b/gi,
+          getHref: (match) => match.startsWith('http') ? match : `https://${match}`
+        },
+        {
+          type: 'email',
+          regex: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
+          getHref: (match) => `mailto:${match}`
+        },
+        {
+          type: 'phone',
+          regex: /\b\+?[0-9]{1,3}?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)*\d{3,4}[-.\s]?\d{3,4}\b/g,
+          getHref: (match) => `tel:${match.replace(/[^\d+]/g, '')}`
+        }
       ];
-      
+
       state.doc.descendants((node, pos) => {
-          if (!node.isText || !node.text) return;
-          
-          const text = node.text;
-          
-          // Supprimer tous les anciens liens
-          node.marks.forEach((mark) => {
-              if (mark.type === linkMark) {
-                  tr.removeMark(pos, pos + node.nodeSize, linkMark);
-                  modified = true;
-              }
-          });
-          
-          textLinkPatterns.forEach(({ regex, getHref }) => {
-              let match;
-              while ((match = regex.exec(text)) !== null) {
-                  const fullMatch = match[0];
-                  const start = match.index;
-                  const end = start + fullMatch.length;
-                  const from = pos + start;
-                  const to = pos + end;
-                  const href = getHref(fullMatch);
-                  
-                  tr.addMark(from, to, linkMark.create({ href }));
-                  modified = true;
-              }
-          });
+        if (!node.isText || !node.text) return;
+
+        const text = node.text;
+
+        if (node.marks.some(mark => mark.type === linkMark)) {
+          tr.removeMark(pos, pos + text.length, linkMark);
+          modified = true;
+        }
+
+        for (const { regex, getHref } of textLinkPatterns) {
+          regex.lastIndex = 0;
+
+          let match;
+          while ((match = regex.exec(text)) !== null) {
+            const fullMatch = match[0];
+            const start = match.index;
+            const end = start + fullMatch.length;
+            const from = pos + start;
+            const to = pos + end;
+            const href = getHref(fullMatch);
+
+            const alreadyHasMark = node.marks.some(mark =>
+              mark.type === linkMark &&
+              mark.attrs.href === href &&
+              start === 0 && end === text.length
+            );
+
+            if (!alreadyHasMark) {
+              tr.addMark(from, to, linkMark.create({ href }));
+              modified = true;
+            }
+          }
+        }
       });
-      
-      if (modified) view.dispatch(tr);
-  }
+
+      if (modified) {
+        view.dispatch(tr);
+      }
+    }
+
 
   /*
    Edit only from within MarkupEditor/rollup/src. After running "npm run build",
