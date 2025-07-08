3a497c1cda2505e94cebda4b9207ef42f62652d0
antoine chinault <antoine@wizzcad.com>
2025-07-08 16:41:13 +0200

    update build

--- MarkupEditor/Resources/markup.js
+++ MarkupEditor/Resources/markup.js
@@ -18253,72 +18253,71 @@
   //  Created by Antoine CHINAULT on 24/06/2025.
   //
 
-    function applyAutoLink(view) {
-      const { state } = view;
-      const { tr } = state;
-      let modified = false;
-      const linkMark = state.schema.marks.link;
-      if (!linkMark) return;
+  function applyAutoLink(view) {
+    const { state } = view;
+    const { tr } = state;
+    let modified = false;
+    const linkMark = state.schema.marks.link;
+    if (!linkMark) return;
 
-      const textLinkPatterns = [
-        {
-          type: 'url',
-          regex: /\b(?:https?:\/\/)?(?:www\.)?[a-z0-9\-._~%]+(?:\.[a-z]{2,})(?:\/[^\s]*)?\b/gi,
-          getHref: (match) => match.startsWith('http') ? match : `https://${match}`
-        },
-        {
-          type: 'email',
-          regex: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
-          getHref: (match) => `mailto:${match}`
-        },
-        {
-          type: 'phone',
-          regex: /\b\+?[0-9]{1,3}?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)*\d{3,4}[-.\s]?\d{3,4}\b/g,
-          getHref: (match) => `tel:${match.replace(/[^\d+]/g, '')}`
-        }
-      ];
+    const textLinkPatterns = [
+      {
+        type: 'url',
+        regex: /\b(?:https?:\/\/)?(?:www\.)?[a-z0-9\-._~%]+(?:\.[a-z]{2,})(?:\/[^\s]*)?\b/gi,
+        getHref: (match) => match.startsWith('http') ? match : `https://${match}`
+      },
+      {
+        type: 'email',
+        regex: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
+        getHref: (match) => `mailto:${match}`
+      },
+      {
+        type: 'phone',
+        regex: /\b\+?[0-9]{1,3}?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)*\d{3,4}[-.\s]?\d{3,4}\b/g,
+        getHref: (match) => `tel:${match.replace(/[^\d+]/g, '')}`
+      }
+    ];
 
-      state.doc.descendants((node, pos) => {
-        if (!node.isText || !node.text) return;
+    state.doc.descendants((node, pos) => {
+      if (!node.isText || !node.text) return;
 
-        const text = node.text;
+      const text = node.text;
 
-        if (node.marks.some(mark => mark.type === linkMark)) {
-          tr.removeMark(pos, pos + text.length, linkMark);
-          modified = true;
-        }
+      if (node.marks.some(mark => mark.type === linkMark)) {
+        tr.removeMark(pos, pos + text.length, linkMark);
+        modified = true;
+      }
 
-        for (const { regex, getHref } of textLinkPatterns) {
-          regex.lastIndex = 0;
+      for (const { regex, getHref } of textLinkPatterns) {
+        regex.lastIndex = 0;
 
-          let match;
-          while ((match = regex.exec(text)) !== null) {
-            const fullMatch = match[0];
-            const start = match.index;
-            const end = start + fullMatch.length;
-            const from = pos + start;
-            const to = pos + end;
-            const href = getHref(fullMatch);
+        let match;
+        while ((match = regex.exec(text)) !== null) {
+          const fullMatch = match[0];
+          const start = match.index;
+          const end = start + fullMatch.length;
+          const from = pos + start;
+          const to = pos + end;
+          const href = getHref(fullMatch);
 
-            const alreadyHasMark = node.marks.some(mark =>
-              mark.type === linkMark &&
-              mark.attrs.href === href &&
-              start === 0 && end === text.length
-            );
+          const alreadyHasMark = node.marks.some(mark =>
+            mark.type === linkMark &&
+            mark.attrs.href === href &&
+            start === 0 && end === text.length
+          );
 
-            if (!alreadyHasMark) {
-              tr.addMark(from, to, linkMark.create({ href }));
-              modified = true;
-            }
+          if (!alreadyHasMark) {
+            tr.addMark(from, to, linkMark.create({ href }));
+            modified = true;
           }
         }
-      });
-
-      if (modified) {
-        view.dispatch(tr);
       }
-    }
+    });
 
+    if (modified) {
+      view.dispatch(tr);
+    }
+  }
 
   /*
    Edit only from within MarkupEditor/rollup/src. After running "npm run build",
@@ -19914,13 +19913,13 @@
   function toggleSuperscript() {
       _toggleFormat('SUP');
   }
-    function triggerEmptyTransaction() {
-      if (window.view && window.view.state) {
-        view.dispatch(view.state.tr.setMeta('force-placeholder', true));
-      } else {
-        console.warn("Editor view not ready in triggerEmptyTransaction");
-      }
+  function triggerEmptyTransaction() {
+    if (window.view && window.view.state) {
+      view.dispatch(view.state.tr.setMeta('force-placeholder', true));
+    } else {
+      console.warn("Editor view not ready in triggerEmptyTransaction");
     }
+  }
 
   function setColor(color, backgroundColor) {
       const markType = view.state.schema.marks.span;
@@ -20721,21 +20720,20 @@
       const state = _getSelectionState();
       return JSON.stringify(state);
   }
-    
-    function getCursorPosition() {
-      const selection = view.state.selection;
-      const coords = view.coordsAtPos(selection.from);
+  function getCursorPosition() {
+    const selection = view.state.selection;
+    const coords = view.coordsAtPos(selection.from);
 
-      const rect = {
-        x: coords.left,
-        y: coords.top,
-        width: coords.right - coords.left,
-        height: coords.bottom - coords.top
-      };
+    const rect = {
+      x: coords.left,
+      y: coords.top,
+      width: coords.right - coords.left,
+      height: coords.bottom - coords.top
+    };
 
-      return rect;
-    }
-    
+    return rect;
+  }
+
   /**
    * Populate a dictionary of properties about the current selection and return it.
    *
@@ -22359,6 +22357,7 @@
   exports.endModalInput = endModalInput;
   exports.focus = focus;
   exports.focusOn = focusOn;
+  exports.getCursorPosition = getCursorPosition;
   exports.getHTML = getHTML;
   exports.getHeight = getHeight;
   exports.getLinkAttributes = getLinkAttributes;
