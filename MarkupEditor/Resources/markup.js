195363f9611c02facdc69b73d946a0a6d74fdf4e
antoine chinault <antoine@wizzcad.com>
2025-07-16 16:55:00 +0200

    [RD-47452] update url regex to check .fr etc

--- MarkupEditor/Resources/markup.js
+++ MarkupEditor/Resources/markup.js
@@ -18260,23 +18260,23 @@
       const linkMark = state.schema.marks.link;
       if (!linkMark) return;
 
-      const textLinkPatterns = [
-        {
-          type: 'url',
-          regex: /\b(?:https?:\/\/)?(?:www\.)?[a-z0-9\-._~%]+(?:\.[a-z]{2,})(?:\/[^\s]*)?\b/gi,
-          getHref: (match) => match.startsWith('http') ? match : `https://${match}`
-        },
-        {
-          type: 'email',
-          regex: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
-          getHref: (match) => `mailto:${match}`
-        },
-        {
-          type: 'phone',
-          regex: /\b\+?[0-9]{1,3}?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)*\d{3,4}[-.\s]?\d{3,4}\b/g,
-          getHref: (match) => `tel:${match.replace(/[^\d+]/g, '')}`
-        }
-      ];
+        const textLinkPatterns = [
+          {
+            type: 'url',
+            regex: /\b(?:https?:\/\/)?(?:www\.)?[a-z0-9\-._~%]+\.[a-z]{2,}(?:\/[^\s]*)?\b/gi,
+            getHref: (match) => match.startsWith('http') ? match : `https://${match}`
+          },
+          {
+            type: 'email',
+            regex: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
+            getHref: (match) => `mailto:${match}`
+          },
+          {
+            type: 'phone',
+            regex: /\b\+?[0-9]{1,3}?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)*\d{3,4}[-.\s]?\d{3,4}\b/g,
+            getHref: (match) => `tel:${match.replace(/[^\d+]/g, '')}`
+          }
+        ];
 
       state.doc.descendants((node, pos) => {
         if (!node.isText || !node.text) return;
